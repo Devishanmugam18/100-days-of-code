6806569bc473229261658504e2c957c78e1c6c23
Devishanmugam18 <79523505+Devishanmugam18@users.noreply.github.com>
2023-05-28 22:27:54 +0530

    Create ArraySortedChecker.java
    Input: N = 5, array[] = {1,2,3,4,5}
    Output: True.
    Explanation: The given array is sorted i.e Every element in the array is smaller than or equals to its next values, So the answer is True.
    
    Algorithm :
    1) As we know that for a sorted array the previous of every element is smaller than or equal to its current element.
    2) So, Through this, we can conclude that if the previous element is smaller than or equal to the current element then. Then we can say that the two elements are sorted. If the condition is true for the entire array then the array is sorted.
    3) We will check every element with its previous element if the previous element is smaller than or equal to the current element then we will move to the next index.
    4) If the whole array is traversed successfully or the size of the given array is zero or one (i.e N = 0 or N = 1). Then we will return True else return False.
    
    Time Complexity: O(N)
    Space Complexity: O(1)

---
+++ ArraySortedChecker.java
@@ -0,0 +1,14 @@
+public class ArraySortedChecker {
+	public static boolean isSorted(int[] array){
+		for(int i=0; i<array.length-1;i++){ 
+			if(array[i] > array[i+1]){
+				return false;
+			}
+		}
+		return true;
+	}
+	public static void main(String[] args){
+		int[] array={1,2,7,4,5};
+		System.out.println("Is array sorted?" + isSorted(array));
+	}
+}
