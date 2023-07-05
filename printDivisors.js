b866ebab604ea8f9e2496dd2718072f5eaf04c8e
Devishanmugam18 <79523505+Devishanmugam18@users.noreply.github.com>
2023-07-05 23:15:30 +0530

    Create printDivisors.js
    Input: n = 36
    Output: 1 2 3 4 6 9 12 18 36
    Explanation: All the divisors of 36 are printed.
    
    Space complexity : O(sqrt(n))
    
    Space Complexity: O(1)

---
+++ printDivisors.js
@@ -0,0 +1,16 @@
+
+function printDivisors(n) {
+  console.log("The Divisors of " + n + " are:");
+  for (let i = 1; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) {
+      process.stdout.write(i + " ");
+      if (i !== n / i) {
+        process.stdout.write(n / i + " ");
+      }
+    }
+  }
+  console.log("\n");
+}
+
+printDivisors(36);
+
