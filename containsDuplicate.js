59902176b1308780413b57e72ff553407c670f1b
Devi Shanmugam <79523505+Devishanmugam18@users.noreply.github.com>
2023-08-11 22:30:35 +0530

    Create containsDuplicate.js
    Input: nums = [1,2,3,1]
    Output: true
    Explanation :  If array contains duplicates return True. If not, return False.
    Time complexity : O(N) 
    Space complexity : O(N)

---
+++ containsDuplicate.js
@@ -0,0 +1,9 @@
+var containsDuplicate = (nums, numsSet = new Set()) => {
+    for (const num of nums) {
+        if (numsSet.has(num)) return true;
+
+        numsSet.add(num);      
+    }
+
+    return false;
+};
