bb76ecb8def08af461f5774a8c60172d4993f9f1
Devi Shanmugam <79523505+Devishanmugam18@users.noreply.github.com>
2023-08-19 23:38:45 +0530

    Create diamondPattern.js
    Input : n =5
    Output :
        
        * 
       * * 
      * * * 
     * * * * 
    * * * * * 
    * * * * * 
     * * * * 
      * * * 
       * * 
        *

---
+++ diamondPattern.js
@@ -0,0 +1,19 @@
+let n = 5;
+
+for (let row = 1; row < 2*n ; row++) {
+    let num = "";
+    let spaceInRow = "";
+
+    let totalColInRow = row > n ? 2 * n - row : row;
+    let spaces = n - totalColInRow;
+    for (let s = 1; s <= spaces; s++){
+        spaceInRow += " ";
+    }
+    console.log(spaceInRow);
+    for (let col = 1; col <= totalColInRow; col++) {
+        num += "* ";
+
+    }
+     console.log(num);
+}
+ 
