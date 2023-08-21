b9e431e4a6cae2dc04041be6721372dab5045c1f
Devi Shanmugam <79523505+Devishanmugam18@users.noreply.github.com>
2023-08-21 13:29:44 +0530

    Update DiamondWithChangingNum.js

--- DiamondWithChangingNum.js
+++ DiamondWithChangingNum.js
@@ -1,28 +1,19 @@
-let n = 7;
-let i = 0;
-let j = 1;
-//print row n times
-for (let row = 1; row <= 2 * n; row++){
-    
-    let rowCopy = row<=n ? row : n-j++;
-    let spaces = "";
-    let num = "";
-    
-    let totalSpaceInRow = row <= n ? n - row : row - n; //calculating total spaces in a row
-    let totalColInRow = row <=n ? row + i++ : (2*n-1) - 2*totalSpaceInRow; //calculating total columns in a row
-    for (let s = 1; s <= totalSpaceInRow; s++){ //print spaces
-        spaces += " " +" "; //append the spaces
+let n = 4;
+
+for (let row = 1; row < 2 * n; row++) { //run the loop 2*n times
+    let spaces = ""; 
+    let changingcolNum = "";
+    let initRowNum = row > n ? 2 * n - row : row; //calculate initial value at each row
+    for (let s = 1; s <= n-initRowNum; s++) { //run the loop till the space count
+        spaces += " "; //append the each space
     }
-    let midIter = (totalColInRow / 2); //calculate mid th iteration
-    
-    for (let c = 1; c <= totalColInRow; c++){ // print columns
-        num += rowCopy+" " ; //append the column number of each row
-        if (c <midIter) { // decrement the number at each column of the row till c is less than mid th iteration
-            rowCopy--;
-        }
-        else {// increment the number at each column of the row after the mid th iteration
-            rowCopy++;
-        }
+    for (let c = initRowNum; c >= 1; c--) { // run the loop from inital row number till 1 
+        changingcolNum += c; //append the number which changes on each iteration
     }
-    console.log(spaces+num); //print the spaces and number at each column of the row
+    for (let c = 2; c <= initRowNum; c++) { // run the loop from 2 till initial row number
+        changingcolNum += c; // append the number which changes on each iteration
+    }
+
+    console.log(spaces + changingcolNum); //print the spaces and changing column number of each row
+
 }
